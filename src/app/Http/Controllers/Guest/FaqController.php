dc3ce153bec3f7cc249f7d4f5732cfd84e7a53a7
Lâm Thành Trung <trunglt@cfox.vn>
2024-08-10 16:47:03 +0700

    update data

---
+++ src/app/Http/Controllers/Guest/FaqController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Guest;
+
+use App\Http\Controllers\Controller;
+
+class FaqController extends Controller
+{
+    /**
+     * Chuyến đến trang câu hỏi thường gặp
+     */
+    public function index() {}
+}
--- src/routes/web.php
+++ src/routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Guest\AboutController;
 use App\Http\Controllers\Guest\BlogController;
 use App\Http\Controllers\Guest\ContactController;
+use App\Http\Controllers\Guest\FaqController;
 use App\Http\Controllers\Guest\HomeController;
 use App\Http\Controllers\Guest\ServiceController;
 use Illuminate\Support\Facades\Route;
@@ -13,9 +14,11 @@ Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('gioi-thieu', [AboutController::class, 'index'])->name('about');
 // trang dịch vụ
 Route::get('dich-vu', [ServiceController::class, 'index'])->name('service');
-// trang bài viết
-Route::get('bai-viet', [BlogController::class, 'index'])->name('blog');
-Route::get('{slug}', [BlogController::class, 'detail'])->name('blog.detail');
+// trang câu hỏi thường gặp
+Route::get('faq', [FaqController::class, 'index'])->name('faq');
 // trang liên hệ
 Route::get('lien-he', [ContactController::class, 'index'])->name('contact');
 Route::post('lien-he', [ContactController::class, 'create'])->name('contact.create');
+// trang bài viết
+Route::get('bai-viet', [BlogController::class, 'index'])->name('blog');
+Route::get('{slug}', [BlogController::class, 'detail'])->name('blog.detail');
