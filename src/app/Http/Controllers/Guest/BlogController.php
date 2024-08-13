dcf463b5c9ed54f9b393d54716f709a6bf6ee3a4
Lâm Thành Trung <trunglt@cfox.vn>
2024-08-13 14:45:20 +0700

    update data

--- src/app/Http/Controllers/Guest/BlogController.php
+++ src/app/Http/Controllers/Guest/BlogController.php
@@ -3,20 +3,36 @@
 namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
+use App\Models\Blog;
+use App\Models\BlogGroup;
 
 class BlogController extends Controller
 {
     /**
      * Chuyến đến trang danh sách bài viết
+     * Hiển thị danh sách danh mục bài viết mới nhất
+     * Hiển thị danh sách bài viết mới nhất
      */
-    public function index() {
-        return view('guest.blog.index');
+    public function index()
+    {
+        $data = [
+            'groups' => BlogGroup::withCount('blogs')->ofStatus(BlogGroup::STATUS_ACTIVE)->latest()->select('id', 'name')->get(),
+            'blogs' => Blog::ofStatus(Blog::STATUS_ACTIVE)->latest()->select('id', 'image', 'name', 'created_at', 'description')->paginate(4)
+        ];
+        return view('guest.blog.index', compact('data'));
     }
 
     /**
      * Chuyến đến trang chi tiết bài viết
+     * Hiển thị thêm 5 bài viết liên quan, cùng danh mục
      */
-    public function detail($slug) {
-        return view('guest.blog.detail');
+    public function detail($slug)
+    {
+        $blog = Blog::ofStatus(Blog::STATUS_ACTIVE)->ofSlug($slug)->firstOrFail();
+        $data = [
+            'blog' => $blog,
+            'other' => Blog::ofStatus(Blog::STATUS_ACTIVE)->where('id', '<>', $blog->id)->where('group_id', $blog->group_id)->select('id', 'slug', 'name', 'image')->limit(5)->get()
+        ];
+        return view('guest.blog.detail', compact('data'));
     }
 }
--- src/app/Http/Controllers/Guest/ContactController.php
+++ src/app/Http/Controllers/Guest/ContactController.php
@@ -3,18 +3,33 @@
 namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
+use App\Models\Contact;
+use Illuminate\Support\Facades\DB;
 
 class ContactController extends Controller
 {
     /**
      * Chuyến đến trang liên hệ
      */
-    public function index() {
+    public function index()
+    {
         return view('guest.contact.index');
     }
 
     /**
      * Hàm lưu thông tin liên hệ
      */
-    public function create() {}
+    public function create()
+    {
+        try {
+            DB::beginTransaction();
+            $data = request()->only('name', 'email', 'comment');
+            Contact::create($data);
+            DB::commit();
+            return redirect()->back()->with('success', 'Gửi liên hệ thành công');
+        } catch (\Throwable $th) {
+            DB::rollBack();
+            return redirect()->back()->with('error', 'Gửi liên hệ thất bại!');
+        }
+    }
 }
